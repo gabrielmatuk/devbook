21f72671bdf5484c65e849313cca47920f520bc4
gabrielmatuk <gabrielmatuk14@gmail.com>
2023-02-14 22:57:54 -0300

    feat(login): create user login

--- app/src/controllers/paginas.go
+++ app/src/controllers/paginas.go
@@ -9,3 +9,9 @@ import (
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
+
+// CarregarPaginaDeCadastroDeUsuario vai renderizar tela de cadastro
+
+func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "cadastro.html", nil)
+}
---
+++ app/src/router/rotas/usuarios.go
@@ -0,0 +1,15 @@
+package rotas
+
+import (
+	"app/src/controllers"
+	"net/http"
+)
+
+var rotasUsuarios = []Rota{
+	{
+		URI:                "/criar-usuario",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.CarregarPaginaDeCadastroDeUsuario,
+		RequerAutenticacao: false,
+	},
+}
