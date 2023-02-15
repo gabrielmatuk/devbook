fdd7e641a95c03e578aebb937eacb5af9da2b1bf
gabrielmatuk <gabrielmatuk14@gmail.com>
2023-02-15 13:06:02 -0300

    feat(login): login screen

--- app/assets/js/cadastro.js
+++ app/assets/js/cadastro.js
@@ -2,7 +2,6 @@ $('#formulario-cadastro').on('submit', criarUsuario)
 
 function criarUsuario(event) {
   event.preventDefault()
-
   if ($('#senha').val() != $('#confirmar-senha').val()) {
     alert('As senhas não coincidem!')
     return
@@ -18,4 +17,11 @@ function criarUsuario(event) {
       senha: $('#senha').val(),
     },
   })
+    .done(function () {
+      alert('Usuário criado com sucesso!')
+    })
+    .fail(function (err) {
+      console.log(err)
+      alert('Erro ao cadastrar usuário.')
+    })
 }
--- app/src/controllers/login.go
+++ app/src/controllers/login.go
@@ -4,16 +4,17 @@ import (
 	"app/src/respostas"
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 )
 
-func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+// FazerLogin utiliza o e-mail e senha do usuário para autenticar o usuário
+func FazerLogin(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	usuario, erro := json.Marshal(map[string]string{
-		"nome":  r.FormValue("nome"),
 		"email": r.FormValue("email"),
-		"nick":  r.FormValue("nick"),
 		"senha": r.FormValue("senha"),
 	})
 	if erro != nil {
@@ -21,18 +22,13 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response, erro := http.Post("http://localhost:5000/usuarios", "application/json", bytes.NewBuffer(usuario))
+	response, erro := http.Post("http://localhost:5000/login", "application/json", bytes.NewBuffer(usuario))
 	if erro != nil {
 		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
 		return
 	}
-	defer response.Body.Close()
 
-	if response.StatusCode >= 400 {
-		respostas.TratarStatusCodeDeErro(w, response)
-		return
-	}
+	token, _ := io.ReadAll(response.Body)
 
-	respostas.JSON(w, response.StatusCode, nil)
-
+	fmt.Println(response.StatusCode, string(token))
 }
